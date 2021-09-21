d940c9e04f9f9bb2049aa6497af1f200fd21c4ea
mWW82t <mWW82t@cyber-dojo.org>
2021-09-21 10:51:49 +0000

    12 ran tests, predicted green, got red

--- files/FizzBuzz.java
+++ files/FizzBuzz.java
@@ -3,8 +3,12 @@ public class FizzBuzz {
 
     public static String fizzbuzz(int number) {
         String result = "";
-        if (number % 3 == 0) result = "Fizz";
-        if (number % 5 == 0) result = "Buzz";
+        if (number % 3 == 0) {
+            result = "Fizz";
+        }
+        if (number % 5 == 0) {
+            result = "Buzz";
+        }
         else result = Integer.toString(number);
         
         return result;
--- files/FizzBuzzTest.java
+++ files/FizzBuzzTest.java
@@ -16,7 +16,7 @@ public class FizzBuzzTest {
     
     @Test
     void shouldReturnFizzForThree(){
-        assertEquals(FizzBuzz.fizzbuzz(3), "HO");
+        assertEquals(FizzBuzz.fizzbuzz(3), "Fizz");
     }
     
     @Test
