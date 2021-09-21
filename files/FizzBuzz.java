14f5c2fd80eb2e4f0340cee3d8c9535faf5e336a
mWW82t <mWW82t@cyber-dojo.org>
2021-09-21 10:48:15 +0000

    6 ran tests, predicted green, got green

--- files/FizzBuzz.java
+++ files/FizzBuzz.java
@@ -3,7 +3,7 @@ public class FizzBuzz {
 
     public static String fizzbuzz(int number) {
         String result = "";
-        if (number % 3 == 0) result = "Fizz"
+        if (number % 3 == 0) result = "Fizz";
         else result = Integer.toString(number);
         
         return result;
